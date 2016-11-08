271ac6915b0e4b8172a51174a81af805bd2bf141
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-11-08 11:54:41 +0100

    Added SongBrowser controller and layout files.

---
+++ src/me/oskarmendel/view/SongBrowserController.java
@@ -0,0 +1,44 @@
+/**
+ * RapidTunes.
+ * The music application to help you use all your music sources in one place.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2016 The RapidTunes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.oskarmendel.view;
+
+import java.util.logging.Logger;
+
+import me.oskarmendel.RapidTunes;
+
+/**
+ * 
+ * @author Oskar
+ * @version 0.00.00
+ * @name SongBrowserController.java
+ */
+public class SongBrowserController implements RapidTunesController {
+	
+	private static final Logger LOGGER = Logger.getLogger(RapidTunes.class.getName());
+
+}
