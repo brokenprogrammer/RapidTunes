eab759bee5151ce1866224abd1555053388cee44
Oskar Mendel <brokenprogrammer@gmail.com>
2017-09-20 08:18:03 +0200

    Finalized all unit tests for BinaryReader.

--- test/me/oskarmendel/util/BinaryReaderTest.java
+++ test/me/oskarmendel/util/BinaryReaderTest.java
@@ -78,6 +78,7 @@ public class BinaryReaderTest {
 			assertEquals("First four bytes in a flac file reads 'fLaC'", 
 					reader.readUnsignedInt(32) == 0x664C6143, true);
 			
+			reader.close();
 		} catch (FileNotFoundException fnf) {
 			fnf.printStackTrace();
 		} catch (IOException io) {
@@ -95,10 +96,13 @@ public class BinaryReaderTest {
 		
 		try {
 			reader = new BinaryReader(nonExistingFile);
-			fail("Expected an FileNotFoundException to be thrown.");
 			
+			fail("Expected an FileNotFoundException to be thrown.");
+			reader.close();
 		} catch (FileNotFoundException e) {
 			assertThat("Expected message is equal.", e.getMessage(), notNullValue());
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -117,6 +121,7 @@ public class BinaryReaderTest {
 			assertEquals("First four bytes in a flac file reads 'fLaC'", 
 					reader.readUnsignedInt(32) == 0x664C6143, true);
 			
+			reader.close();
 		} catch (IOException io) {
 			io.printStackTrace();
 		}
@@ -137,8 +142,9 @@ public class BinaryReaderTest {
 			assertEquals("First byte in a flac file reads 'a'", 'a', reader.readUnsignedInt(8));
 			assertEquals("First byte in a flac file reads 'C'", 'C', reader.readUnsignedInt(8));
 
-			assertThat("Read bytes is matching actual read bytes.", reader.getPosition(), is(4));
+			assertThat("Read bytes is matching actual read bytes.", reader.getPosition(), is(4L));
 			
+			reader.close();
 		} catch (IOException io) {
 			io.printStackTrace();
 		}
@@ -152,6 +158,7 @@ public class BinaryReaderTest {
 		File flacFile = new File("./demo/Jimmy Pengiun - Untitled Star.flac");
 		BinaryReader reader = null;
 		
+		// Test reading a value too large for an unsigned it.
 		try {
 			reader = new BinaryReader(flacFile);
 			assertEquals("First byte in a flac file reads 'f'", 'f', reader.readUnsignedInt(8));
@@ -162,8 +169,9 @@ public class BinaryReaderTest {
 			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 4);
 			
 			reader.readUnsignedInt(35);
-			fail("Exception should be catched.");
 			
+			fail("Exception should be catched.");
+			reader.close();
 		} catch (IllegalArgumentException ar) {
 			assertThat("Expected message is equal.", ar.getMessage(), 
 					is("Specified number of bits (35) does not fit in an unsigned integer."));
@@ -171,6 +179,7 @@ public class BinaryReaderTest {
 			e.printStackTrace();
 		}
 		
+		// Test Reading until EOF.
 		try {
 			if (reader != null) {
 				reader.seekTo(reader.getLength());
@@ -185,6 +194,7 @@ public class BinaryReaderTest {
 				fail("BinaryReader is not initialized.");
 			}
 			
+			reader.close();
 		} catch (IllegalStateException e) {
 			assertThat("Expected message is equal.", e.getMessage(), 
 					is("Reached EOF."));
@@ -193,39 +203,162 @@ public class BinaryReaderTest {
 		}
 	}
 
+	/**
+	 * Test method for {@link me.oskarmendel.util.BinaryReader#readSignedInt(int)}
+	 */
 	@Test
 	public final void testReadSignedInt() {
-		fail("Not yet implemented");
+		File flacFile = new File("./demo/Jimmy Pengiun - Untitled Star.flac");
+		BinaryReader reader = null;
+		
+		try {
+			reader = new BinaryReader(flacFile);
+			assertEquals("First byte in a flac file reads 'f'", 'f', reader.readSignedInt(8));
+			assertEquals("First byte in a flac file reads 'L'", 'L', reader.readSignedInt(8));
+			assertEquals("First byte in a flac file reads 'a'", 'a', reader.readSignedInt(8));
+			assertEquals("First byte in a flac file reads 'C'", 'C', reader.readSignedInt(8));
+			
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 4);
+			
+			int i = 0;
+			while (i == 0) {
+				i = reader.readSignedInt(8);
+			}
+			
+			assertThat("First unsigned value after 'fLaC' tag is 32", i, is(34));
+			
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * Test method for {@link me.oskarmendel.util.BinaryReader#readByte()}
+	 */
 	@Test
 	public final void testReadByte() {
-		fail("Not yet implemented");
+		File flacFile = new File("./demo/Jimmy Pengiun - Untitled Star.flac");
+		BinaryReader reader = null;
+		
+		try {
+			reader = new BinaryReader(flacFile);
+			
+			assertEquals("First byte in a flac file reads 'f'", 'f', reader.readByte());
+			assertEquals("First byte in a flac file reads 'L'", 'L', reader.readByte());
+			assertEquals("First byte in a flac file reads 'a'", 'a', reader.readByte());
+			assertEquals("First byte in a flac file reads 'C'", 'C', reader.readByte());
+			
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 4);
+			
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * Test method for {@link me.oskarmendel.util.BinaryReader#close()}
+	 */
 	@Test
 	public final void testClose() {
-		fail("Not yet implemented");
+		File flacFile = new File("./demo/Jimmy Pengiun - Untitled Star.flac");
+		BinaryReader reader = null;
+		
+		try {
+			reader = new BinaryReader(flacFile);
+			
+			assertEquals("First byte in a flac file reads 'f'", 'f', reader.readByte());
+			assertEquals("First byte in a flac file reads 'L'", 'L', reader.readByte());
+			assertEquals("First byte in a flac file reads 'a'", 'a', reader.readByte());
+			assertEquals("First byte in a flac file reads 'C'", 'C', reader.readByte());
+			
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 4);
+			
+			reader.close();
+			assertThat("Position is invalidated.", reader.getPosition(), is(-1L));
+			assertThat("Bit position is invalidated.", reader.getBitPosition(), is(-1));
+			assertThat("Length is invalidated.", reader.getLength(), is(-1));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * Test method for {@link me.oskarmendel.util.BinaryReader#seekTo(long)}
+	 */
 	@Test
 	public final void testSeekTo() {
-		fail("Not yet implemented");
+		File flacFile = new File("./demo/Jimmy Pengiun - Untitled Star.flac");
+		BinaryReader reader = null;
+		
+		try {
+			reader = new BinaryReader(flacFile);
+			
+			reader.seekTo(1);
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 1);
+			
+			reader.seekTo(2);
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 2);
+			
+			reader.seekTo(3);
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 3);
+			
+			reader.seekTo(4);
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 4);
+			
+			reader.seekTo(50000);
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 50000);
+			
+			reader.seekTo(5758978);
+			assertEquals("Read bytes is matching actual read bytes.", reader.getPosition(), 5758978);
+			
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Test
-	public final void testGetPosition() {
-		fail("Not yet implemented");
-	}
-
+	/**
+	 * Test method for {@link me.oskarmendel.util.BinaryReader#getBitPosition()}
+	 */
 	@Test
 	public final void testGetBitPosition() {
-		fail("Not yet implemented");
+		File flacFile = new File("./demo/Jimmy Pengiun - Untitled Star.flac");
+		BinaryReader reader;
+		
+		try {
+			reader = new BinaryReader(flacFile);
+			
+			int i = 0;
+			
+			i = reader.readUnsignedInt(1);
+			assertEquals("Current bit position: 1", reader.getBitPosition(), 1);
+			
+			i = reader.readUnsignedInt(1);
+			assertEquals("Current bit position: 2", reader.getBitPosition(), 2);
+			
+			i = reader.readUnsignedInt(1);
+			assertEquals("Current bit position: 3", reader.getBitPosition(), 3);
+			
+			i = reader.readUnsignedInt(1);
+			assertEquals("Current bit position: 4", reader.getBitPosition(), 4);
+			
+			i = reader.readUnsignedInt(2);
+			assertEquals("Current bit position: 6", reader.getBitPosition(), 6);
+			
+			i = reader.readUnsignedInt(3);
+			assertEquals("Current bit position: 1", reader.getBitPosition(), 1);
+			
+			i = reader.readUnsignedInt(6);
+			assertEquals("Current bit position: 7", reader.getBitPosition(), 7);
+			
+			i = reader.readUnsignedInt(13);
+			assertEquals("Current bit position: 4", reader.getBitPosition(), 4);
+			
+			reader.close();
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
 	}
-
-	@Test
-	public final void testGetLength() {
-		fail("Not yet implemented");
-	}
-
 }
