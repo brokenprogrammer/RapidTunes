e7d97b445ecf227ca2099abccd6f78641f563a65
Oskar Mendel <brokenprogrammer@gmail.com>
2021-03-09 19:48:48 +0100

    Fresh template.

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = (): void => {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      worldSafeExecuteJavaScript: true,
+      plugins: true,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("ready", createWindow);
