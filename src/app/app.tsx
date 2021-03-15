c41a8e26c574e2d2959376ebb2b4db07ab419868
Oskar <brokenprogrammer@gmail.com>
2021-03-15 13:50:36 +0100

    Removed unused ts-ignores.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -24,47 +24,41 @@ const MainView = () => {
   };
 
   const handleScriptLoad = () => {
-    // @ts-ignore
     window.onSpotifyWebPlaybackSDKReady = () => {
       const token =
         "BQCb4e7uRbh0AadeMfAyMewOyId75orPHjsbOSzMPZhKaP3fQn3wMAwLnQDXTidgFklLrdXKkyjWEPhLErN7H1QBEPC1YMMjFi55UzxxN4dPkF4BgtwWpg_D6LwmSmY8vPi0MMPCk9-cXLNqMFEf9y97n-eDZo7R1dKINNm6O_w-Lg";
-      // @ts-ignore
+
       const player = new window.Spotify.Player({
         name: "RapidTunes",
-        // @ts-ignore
         getOAuthToken: (cb) => {
           cb(token);
         },
       });
 
-      // @ts-ignore
       player.addListener("initialization_error", ({ message }) => {
         console.error(message);
       });
-      // @ts-ignore
+
       player.addListener("authentication_error", ({ message }) => {
         console.error(message);
       });
-      // @ts-ignore
+
       player.addListener("account_error", ({ message }) => {
         console.error(message);
       });
-      // @ts-ignore
+
       player.addListener("playback_error", ({ message }) => {
         console.error(message);
       });
 
-      // @ts-ignore
       player.addListener("player_state_changed", (state) => {
         console.log(state);
       });
 
-      // @ts-ignore
       player.addListener("ready", ({ device_id }) => {
         console.log("Ready with Device ID", device_id);
       });
 
-      // @ts-ignore
       player.addListener("not_ready", ({ device_id }) => {
         console.log("Device ID has gone offline", device_id);
       });
